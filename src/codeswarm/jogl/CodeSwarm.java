631b5543aad3f508efb330dcc7533430dc0eb4a4
arjenw <arjenw@eda9c206-d64f-0410-a2f2-67f3fa0499ed>
2008-06-27 22:02:12 +0000

    Setup a basic AWT panel + JOGL panel, adding a windowClosing handler
    git-svn-id: http://codeswarm.googlecode.com/svn/trunk@67 eda9c206-d64f-0410-a2f2-67f3fa0499ed

--- src/codeswarm/jogl/CodeSwarm.java
+++ src/codeswarm/jogl/CodeSwarm.java
@@ -21,22 +21,87 @@ package codeswarm.jogl;
 	 along with code_swarm.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+import javax.media.opengl.GLEventListener;
+import javax.media.opengl.GLAutoDrawable;
+import javax.media.opengl.GLCanvas;
+import javax.media.opengl.GL;
+import javax.media.opengl.GLJPanel;
+// import javax.media.opengl.DebugGL;
 import java.util.logging.Logger;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 /**
  * This is the Java OpenGL version of the original processing code_swarm application
- *
+ * <p/>
  * It is modelled after the original prototype.
  *
  * @author <a href="mailto:arjenw@gmail.com">Arjen Wiersma</a>
  */
-public class CodeSwarm {
+public class CodeSwarm implements GLEventListener {
     public static final Logger log = Logger.getLogger(CodeSwarm.class.getName());
+
+    public CodeSwarm() {
+
+    }
+
+    public void init(GLAutoDrawable glAutoDrawable) {
+        // Use debug pipeline
+        //glAutoDrawable.setGL(new DebugGL(glAutoDrawable.getGL()));
+
+        GL gl = glAutoDrawable.getGL();
+        gl.setSwapInterval(1);
+    }
+
+    public void display(GLAutoDrawable glAutoDrawable) {
+        GL gl = glAutoDrawable.getGL();
+
+        if ((glAutoDrawable instanceof GLJPanel) && !((GLJPanel) glAutoDrawable).isOpaque() && ((GLJPanel) glAutoDrawable).shouldPreserveColorBufferIfTranslucent()) {
+            gl.glClear(GL.GL_DEPTH_BUFFER_BIT);
+        }
+        else {
+            gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
+        }
+    }
+
+    public void reshape(GLAutoDrawable glAutoDrawable, int x, int y, int width, int height) {
+        GL gl = glAutoDrawable.getGL();
+
+        // ratio...
+        // float h = (float) height / (float) width;
+
+        gl.glMatrixMode(GL.GL_PROJECTION);
+
+    }
+
+    public void displayChanged(GLAutoDrawable glAutoDrawable, boolean modeChanged, boolean deviceChanged) { }
 
     public static void main(String[] args) {
         log.info("Running CodeSwarm with parameters: ");
         for (String arg : args) {
             log.info("Argument: " + arg);
         }
+
+        Frame frame = new Frame("CodeSwarm JOGL");
+        GLCanvas canvas = new GLCanvas();
+        canvas.addGLEventListener(new CodeSwarm());
+        frame.add(canvas);
+        // set the size
+        frame.setSize(640, 480);
+        // also add something to stop the animation thread
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                log.info("Closing the window");
+                new Thread(new Runnable() {
+                    public void run() {
+                        // stop the animator here...
+                        System.exit(0);
+                    }
+                }).start();
+            }
+        });
+        frame.setVisible(true);
     }
 }
