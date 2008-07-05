921ab49750c630f2412ce34da6918ed3fa0c248c
sebastien.rombauts <sebastien.rombauts@eda9c206-d64f-0410-a2f2-67f3fa0499ed>
2008-07-05 13:54:21 +0000

    moving call to ant build after the test for help request
    git-svn-id: http://codeswarm.googlecode.com/svn/trunk@118 eda9c206-d64f-0410-a2f2-67f3fa0499ed

--- run.sh
+++ run.sh
@@ -5,29 +5,10 @@ params=$@
 default_config="data/sample.config"
 code_swarm_jar="dist/code_swarm.jar"
 
-if [ -f $code_swarm_jar ]; then
-    echo "code_swarm project !"
-else
-    echo "no code swarm binaries !"
-    echo "needing to build it with ant and javac (java-sdk)"
-    echo ""
-    echo "auto-trying the ant command..."
-    if ant; then
-        echo ""
-        echo "code_swarm project !"
-    else
-        echo ""
-        echo "ERROR, please verify 'ant' and 'java-sdk' installation"
-        echo -n "press a key to exit"
-        read key
-        echo "bye"
-        exit
-    fi
-fi
-
 # command line parameters basic check
 if [ $# = 0 ]; then
     # asking user for a config file
+    echo "code_swarm project !"
     echo -n "Specify a config file, or ENTER for default one [$default_config] : "
     read config
     if [ ${#config} = 0 ]; then
@@ -43,11 +24,30 @@ else
         echo "   data/sample.config  is the default config file"
         echo ""
         exit
+    else
+        echo "code_swarm project !"
     fi
 fi
 
-# else, running
+# checking for code_swarm java binaries
+if [ ! -f $code_swarm_jar ]; then
+    echo "no code_swarm binaries !"
+    echo "needing to build it with 'ant' and 'javac' (java-sdk)"
+    echo ""
+    echo "auto-trying the ant command..."
+    if ant; then
+        echo ""
+    else
+        echo ""
+        echo "ERROR, please verify 'ant' and 'java-sdk' installation"
+        echo -n "press a key to exit"
+        read key
+        echo "bye"
+        exit
+    fi
+fi
 
+# running
 if java -Xmx1000m -classpath dist/code_swarm.jar:lib/core.jar:lib/xml.jar:. code_swarm $params; then
 # always on error due to no "exit buton" on rendering window
     echo "bye"
@@ -56,6 +56,5 @@ if java -Xmx1000m -classpath dist/code_swarm.jar:lib/core.jar:lib/xml.jar:. code
 else
     echo "bye"
 fi
-
 
 
