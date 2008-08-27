d0406add8afe1029d822da34b6151d2506e51828
Peter Burns <peter@metaweb.com>
2008-08-27 00:28:05 -0700

    Added a script to sort xml event files
    Also sorted the sample event file

---
+++ convert_logs/sort_code_swarm_input.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+
+#sorts a code_swarm xml file
+#assumes: one <event> element per line
+#         the string "<event" is in each <event> line and only in <event> lines
+#         the xml isn't pathological
+
+import sys, re
+
+try:
+    xml_lines = open(sys.argv[1])
+except Exception, e:
+    xml_lines = sys.stdin
+
+xml_lines = filter(lambda line: "<event" in line, xml_lines)
+
+date_extractor = re.compile(r".*date\s*=\s*(\'|\")(\d+)(\'|\").*")
+def get_date_value(line):
+    return int(date_extractor.search(line).group(2))
+xml_lines.sort(key=get_date_value)
+
+print """<?xml version="1.0"?>
+<file_events>
+%s
+</file_events> """ % "".join(xml_lines)
