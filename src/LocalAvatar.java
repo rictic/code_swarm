478f1cafc7a6345fdcbb29d46cdf0a2e2b5806a2
Frederic Back <fredericback@gmail.com>
2009-03-27 19:39:10 -0700

    Added experimental support for local avatars.
    Put png files in data/local_avatars rather than load them from a
    web service.

---
+++ src/LocalAvatar.java
@@ -0,0 +1,21 @@
+import java.io.File;
+
+public class LocalAvatar extends AvatarFetcher {
+  public String dir = "";
+  public String empty_pic = null;
+
+  public LocalAvatar(CodeSwarmConfig cfg) {  
+    super(cfg);
+    dir = cfg.getStringProperty("LocalAvatarDirectory");  
+    empty_pic = cfg.getStringProperty("LocalAvatarDefaultPic");     
+  }
+
+  public String fetchUserImage(String username) {
+    String filename = dir+username+".png";        
+    File f = new File(filename);
+    if(f.exists()) return filename;
+    return dir+empty_pic;
+  }
+  
+
+}
