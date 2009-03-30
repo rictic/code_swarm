f046d896fb1555ea570b322ac1ef54c59b52a5fd
Peter Burns <peter@metaweb.com>
2009-03-29 17:51:55 -0700

    Handle "First Last <email@example.com>" format for gravatars

--- src/GravatarFetcher.java
+++ src/GravatarFetcher.java
@@ -1,5 +1,7 @@
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class GravatarFetcher extends AvatarFetcher {
   public GravatarFetcher(CodeSwarmConfig cfg) {
@@ -7,12 +9,21 @@ public class GravatarFetcher extends AvatarFetcher {
   }
 
   public String fetchUserImage(String username) {
-    String hash = md5Hex(username);
+    String email = getEmail(username);
+    String hash = md5Hex(email);
     try {
       return getImage(hash, new URL("http://www.gravatar.com/avatar/" + hash + "?d="+cfg.getStringProperty("GravatarFallback")+ "&s=" + size));
     } catch (MalformedURLException e) {
       e.printStackTrace(); //should be impossible...
       return null;
     }
+  }
+  
+  private Pattern emailPattern = Pattern.compile("<(.*?@.*?\\..*?)>");
+  private String getEmail(String username) {
+    Matcher emailMatcher = emailPattern.matcher(username);
+    if (emailMatcher.find())
+      return username.substring(emailMatcher.start(1), emailMatcher.end(1));
+    return username;
   }
 }
